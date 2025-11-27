4ff9168d49d6bddd36c3af8ddf9cd952725b6dc1
digjhonson <diego.slira2@senacsp.edu.br>
2025-11-27 11:55:02 -0300

    edu

--- script.js
+++ script.js
@@ -33,107 +33,17 @@ const irVarandaTexto = document.getElementById("ir-varanda-texto");
 
 // ################# Alterando estado dos dispositivos
 
-// pegando o evento de mudança do botão (ligado/desligado)
-lampSalaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = lampSalaInput.checked === true;
 
-    if (ligado === true){
-        console.log("Lampada ligada");
-        lampSalaTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Lampada desligada");
-        lampSalaTexto.innerHTML = "Desligado";
-    }
-});
 
-cortSalaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = cortSalaInput.checked === true;
 
-    if (ligado === true){
-        cortSalaTexto.innerHTML = "Aberta";
-    } else {
-        cortSalaTexto.innerHTML = "Fechada";
-    }
-});
 
-portSalaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = portSalaInput.checked === true;
 
-    if (ligado === true){
-        portSalaTexto.innerHTML = "Aberta";
-    } else {
-        portSalaTexto.innerHTML = "Fechada";
-    }
-});
 
-lampCozinhaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = lampCozinhaInput.checked === true;
 
-    if (ligado === true){
-        lampCozinhaTexto.innerHTML = "Ligada";
-    } else {
-        lampCozinhaTexto.innerHTML = "Desligada";
-    }
-});
 
-exausCozinhaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = exausCozinhaInput.checked === true;
 
-    if (ligado === true){
-        exausCozinhaTexto.innerHTML = "Ligada";
-    } else {
-        exausCozinhaTexto.innerHTML = "Desligada";
-    }
-});
 
-lampVarandaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = lampVarandaInput.checked === true;
 
-    if (ligado === true){
-        lampVarandaTexto.innerHTML = "Ligada";
-    } else {
-        lampVarandaTexto.innerHTML = "Desligada";
-    }
-});
-
-varVarandaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = varVarandaInput.checked === true;
-
-    if (ligado === true){
-        varVarandaTexto.innerHTML = "Aberto";
-    } else {
-        varVarandaTexto.innerHTML = "Fechado";
-    }
-});
-
-portVarandaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = portVarandaInput.checked === true;
-
-    if (ligado === true){
-        portVarandaTexto.innerHTML = "Aberto";
-    } else {
-        portVarandaTexto.innerHTML = "Fechado";
-    }
-});
-
-irVarandaInput.addEventListener("change", () => {
-    // verifica de o botão esta como ligado
-    const ligado = irVarandaInput.checked === true;
-
-    if (ligado === true){
-        irVarandaTexto.innerHTML = "Ligada";
-    } else {
-        irVarandaTexto.innerHTML = "Desligada";
-    }
-});
 
 
 
@@ -148,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Cliente conectado: ", clienteId);
 
         // ### criando um topico para acessar mensagens do MQTT
-        const topicoTeste = 'teste/ingrid';
+        const topicoTeste = 'teste/diego';
 
         // ### recebendo mensagens do topico criado
         cliente.subscribe(topicoTeste);
@@ -163,6 +73,147 @@ document.addEventListener("DOMContentLoaded", () => {
     // ### Recebendo as mensagem dos tópicos assinados no MQTT pelo cliente
     cliente.on("message", (topico, mensagem) => {
         console.log("Topico recebido: ", topico);
-        console.log("Mensagem recebida: ", mensagem);
+        console.log("Mensagem recebida: ", mensagem.toString());
     });
+
+
+    // pegando o evento de mudança do botão (ligado/desligado)
+    lampSalaInput.addEventListener("change", () => {
+
+        const TOPICO_LAMP_SALA = 'casa-diego/sala/lamp';
+
+        // verifica de o botão esta como ligado
+        const ligado = lampSalaInput.checked === true;
+
+        if (ligado === true) {
+            //console.log("Lampada ligada");
+            cliente.publish(TOPICO_LAMP_SALA, 'Ligado')
+            lampSalaTexto.innerHTML = "Ligado";
+        } else {
+            //console.log("Lampada desligada");
+            cliente.publish(TOPICO_LAMP_SALA, 'Desligado')
+            lampSalaTexto.innerHTML = "Desligado";
+        }
+    });
+
+
+    cortSalaInput.addEventListener("change", () => {
+        const TOPICO_CORT_SALA = 'casa-diego/sala/cort';
+
+        // verifica de o botão esta como ligado
+        const ligado = cortSalaInput.checked === true;
+
+        if (ligado === true) {
+            cliente.publish(TOPICO_CORT_SALA, 'Ligado')
+            cortSalaTexto.innerHTML = "Aberta";
+        } else {
+            cliente.publish(TOPICO_CORT_SALA, 'Desligado')
+            cortSalaTexto.innerHTML = "Fechada";
+        }
+    });
+
+    portSalaInput.addEventListener("change", () => {
+        const TOPICO_PORT_SALA = 'casa-diego/sala/port';
+        // verifica de o botão esta como ligado
+        const ligado = portSalaInput.checked === true;
+
+        if (ligado === true) {
+            cliente.publish(TOPICO_PORT_SALA, 'Ligado')
+            portSalaTexto.innerHTML = "Aberta";
+        } else {
+            cliente.publish(TOPICO_PORT_SALA, 'Desligado')
+            portSalaTexto.innerHTML = "Fechada";
+        }
+    });
+
+    lampCozinhaInput.addEventListener("change", () => {
+           const TOPICO_LAMP_COZINHA = 'casa-diego/cozinha/lamp';
+    // verifica de o botão esta como ligado
+    const ligado = lampCozinhaInput.checked === true;
+
+    if (ligado === true) {
+         cliente.publish(TOPICO_LAMP_COZINHA, 'Ligado')
+        lampCozinhaTexto.innerHTML = "Ligada";
+    } else {
+         cliente.publish(TOPICO_LAMP_COZINHA, 'Desligado')
+        lampCozinhaTexto.innerHTML = "Desligada";
+    }
+});
+
+exausCozinhaInput.addEventListener("change", () => {
+    const TOPICO_EXAUS_COZINHA = 'casa-diego/cozinha/exaus';
+    // verifica de o botão esta como ligado
+    const ligado = exausCozinhaInput.checked === true;
+
+    if (ligado === true) {
+        cliente.publish(TOPICO_EXAUS_COZINHA, 'Ligado')
+        exausCozinhaTexto.innerHTML = "Ligada";
+    } else {
+         cliente.publish(TOPICO_EXAUS_COZINHA, 'Desligado')
+        exausCozinhaTexto.innerHTML = "Desligada";
+    }
+});
+
+lampVarandaInput.addEventListener("change", () => {
+    
+    const TOPICO_LAMP_VARANDA = 'casa-diego/varanda/lamp';
+    // verifica de o botão esta como ligado
+    const ligado = lampVarandaInput.checked === true;
+
+    if (ligado === true) {
+        cliente.publish(TOPICO_LAMP_VARANDA, 'Ligado')
+        lampVarandaTexto.innerHTML = "Ligada";
+    } else {
+         cliente.publish(TOPICO_LAMP_VARANDA, 'Desligado')
+        lampVarandaTexto.innerHTML = "Desligada";
+    }
+});
+
+varVarandaInput.addEventListener("change", () => {
+
+     const TOPICO_VAR_VARANDA = 'casa-diego/varanda/var'
+
+    // verifica de o botão esta como ligado
+    const ligado = varVarandaInput.checked === true;
+
+    if (ligado === true) {
+        cliente.publish(TOPICO_VAR_VARANDA, 'Ligado')
+        varVarandaTexto.innerHTML = "Aberto";
+    } else {
+         cliente.publish(TOPICO_VAR_VARANDA, 'Desligado')
+        varVarandaTexto.innerHTML = "Fechado";
+    }
+});
+
+portVarandaInput.addEventListener("change", () => {
+
+     const TOPICO_port_VARANDA = 'casa-diego/varanda/port'
+
+    // verifica de o botão esta como ligado
+    const ligado = portVarandaInput.checked === true;
+
+    if (ligado === true) {
+         cliente.publish(TOPICO_PORT_VARANDA, 'Aberta')
+        portVarandaTexto.innerHTML = "Aberto";
+    } else {
+         cliente.publish(TOPICO_PORT_VARANDA, 'Fechada')
+        portVarandaTexto.innerHTML = "Fechado";
+    }
+});
+
+irVarandaInput.addEventListener("change", () => {
+
+     const TOPICO_IR_VARANDA = 'casa-diego/varanda/ir'
+
+    // verifica de o botão esta como ligado
+    const ligado = irVarandaInput.checked === true;
+
+    if (ligado === true) {
+         cliente.publish(TOPICO_IR_VARANDA, 'Ligada')
+        irVarandaTexto.innerHTML = "Ligada";
+    } else {
+           cliente.publish(TOPICO_IR_VARANDA, 'Desligada')
+        irVarandaTexto.innerHTML = "Desligada";
+    }
+});
 });
